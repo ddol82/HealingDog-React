9baa98f48b8a9a6d9a05631c80bc22741e78a2ec
ddol82 <wlssud@mensakorea.org>
2023-03-20 15:57:31 +0900

    체크아웃 테스트 (master -> feature)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1>healingdog's first step</h1>
       <p>add "p" tag to check the message in slack!</p>
+      <p>and checkout test to my feature branch</p>
     </div>
   );
 }
