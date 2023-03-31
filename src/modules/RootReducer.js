6a4721ee5db0f9a1f75b39c6000251ca1d13898c
cymon337 <113357179+cymon337@users.noreply.github.com>
2023-03-31 15:59:11 +0900

    ✨ feat(login): 로그인기능임시구현

--- src/modules/RootReducer.js
+++ src/modules/RootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import boardingReducer from "./BoardingModule";
 import reviewReducer from "./ReviewModule";
+import memberReducer from "./MemberModule";
 
 
 const rootReducer = combineReducers({
+    memberReducer,
     reviewReducer,
     boardingReducer
 });
