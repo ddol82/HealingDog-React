9c6f459fe53614e0b2f31b334401578854aae4de
김선중 <sjkim3515@gmail.com>
2023-04-07 10:00:41 +0900

    🎉 init: 0407 아침커밋푸쉬
    로그인 css 화면구현 완료

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,130 +1,149 @@
 import { NavLink } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, Navigate } from 'react-router-dom';
-import { callLoginUserAPI } from '../../apis/MemberAPICalls';
-import { callLoginProviderAPI } from '../../apis/MemberAPICalls'
+import { useNavigate, Navigate } from "react-router-dom";
+import { callLoginUserAPI } from "../../apis/MemberAPICalls";
+import { callLoginProviderAPI } from "../../apis/MemberAPICalls";
+import "../../styles/Login.css";
 
 function Login() {
+  //필요기능선언
+  // url 이동을 위한 네비게이트 선언
+  const navigate = useNavigate();
 
-//필요기능선언
-    // url 이동을 위한 네비게이트 선언
-	const navigate = useNavigate();
+  /* dispatcher, selector 선언 */
+  // 리액트 스토어의 dispatch 를 함수로 사용할수 있게 해주는 hook
+  const dispatch = useDispatch();
+  const loginMember = useSelector((state) => state.memberReducer);
+  // index.js - <Provider store={ store }>
+  // store - const store = createStore(rootReducer, ...);
+  // const rootReducer = combineReducers({memberReducer, ...});
+  // loginMember = memberReducer(src/modules/MemberModules.js)
 
-    /* dispatcher, selector 선언 */
-    // 리액트 스토어의 dispatch 를 함수로 사용할수 있게 해주는 hook
-	const dispatch = useDispatch();
-	const loginMember = useSelector(state => state.memberReducer);
-	// index.js - <Provider store={ store }>
-	// store - const store = createStore(rootReducer, ...);
-	// const rootReducer = combineReducers({memberReducer, ...});
-	// loginMember = memberReducer(src/modules/MemberModules.js)
+  // state 선언 컴포넌트 내부 사용
+  // 폼 데이터 State 로 관리
+  const [form, setForm] = useState({
+    type: "user",
+    email: "",
+    password: "",
+  });
 
-// state 선언 컴포넌트 내부 사용
-    // 폼 데이터 State 로 관리
-    const [form, setForm] = useState({
-		type: 'user',
-		email: '',
-		password: ''
+  // useEffect 를 이용한 component logic 처리
+  useEffect(
+    () => {
+      if (loginMember.status === 200) {
+        console.log("[Login] Login SUCCESS {}", loginMember);
+        navigate("/", { replace: true });
+      }
+    }, // eslint-disable-next-line
+    [loginMember]
+  ); // loginMember 변화 감지
+
+  // eslint-disable-next-line
+  if (!!window.localStorage.getItem("accessToken")) {
+    console.log("[Login] Login is already authenticated by the server");
+    return <Navigate to="/" />;
+  }
+
+  // event handler
+
+  // useState 에 input 변화값 저장
+  const onChangeHandler = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
     });
+  };
 
-// useEffect 를 이용한 component logic 처리
-    useEffect(() => { 
-        if(loginMember.status === 200){
-            console.log("[Login] Login SUCCESS {}", loginMember);
-            navigate("/", { replace: true });
-        }
-    } // eslint-disable-next-line
-    ,[loginMember]); // loginMember 변화 감지
-
-	// eslint-disable-next-line
-    if(!!window.localStorage.getItem('accessToken')) {
-        console.log("[Login] Login is already authenticated by the server");        
-        return <Navigate to="/"/>
+  // 로그인 버튼 클릭시 디스패처 실행 및 메인 페이지로 이동
+  const onClickLoginHandler = () => {
+    switch (form.type) {
+      case "user":
+        dispatch(
+          callLoginUserAPI({
+            // 로그인 유저
+            form: form,
+          })
+        );
+        break;
+      case "provider":
+        dispatch(
+          callLoginProviderAPI({
+            // 로그인 제공자
+            form: form,
+          })
+        );
     }
+  };
+  // form의 key => email, password
 
-// event handler
-
-    // useState 에 input 변화값 저장
-    const onChangeHandler = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    // 로그인 버튼 클릭시 디스패처 실행 및 메인 페이지로 이동
-    const onClickLoginHandler = () => {
-		switch(form.type) {
-			case "user" :
-				dispatch(callLoginUserAPI({	// 로그인 유저
-					form: form
-				}));
-				break;
-			case "provider" :
-				dispatch(callLoginProviderAPI({	// 로그인 제공자
-					form: form
-				}));
-		}
-    }
-	// form의 key => email, password
-
-	return (
-		<>
-			<div>
-				<h1>HEALING DOG</h1>
-			</div>
-			<div>
-				<label>
-					<input
-						type="radio"
-						name="type"
-						value="user"
-						defaultChecked="true"
-						onClick={ onChangeHandler }
-					/>사용자 로그인
-				</label>
-				<label>
-					<input
-						type="radio"
-						name="type"
-						value="provider"
-						onClick={ onChangeHandler }
-					/>제공자 로그인
-				</label>
-			</div>
-			<div>
-				<div>
-					<input
-						type="text"
-						name="email"
-						placeholder="이메일"
-						autoComplete="off"
-						onChange={ onChangeHandler }
-					/>
-					</div>
-				<div>
-					<input
-						type="password"
-						name="password"
-						placeholder="비밀번호"
-						autoComplete="off"
-						onChange={ onChangeHandler }
-					/>
-				</div>
-				<div>
-					<NavLink>이메일찾기</NavLink>
-					<NavLink>비밀번호찾기</NavLink>
-					<NavLink>회원가입</NavLink>
-				</div>
-				<div>
-					<button
-						onClick={ onClickLoginHandler }
-					>로그인</button>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className="login-container">
+      <main className="login-main">
+        <div className="login-center">
+          <div className="lgoin-titlename">
+            <h1>HEALING DOG</h1>
+          </div>
+          <div className="login-checkmember">
+            <input
+              className="login-check"
+              id="id-check-01"
+              type="radio"
+              name="type"
+              value="user"
+              defaultChecked="true"
+              onClick={onChangeHandler}
+            />
+            <label htmlFor="id-check-01">사용자 로그인</label>
+            <input
+              className="login-check"
+              id="id-check-02"
+              type="radio"
+              name="type"
+              value="provider"
+              onClick={onChangeHandler}
+            />
+            <label htmlFor="id-check-02">제공자 로그인</label>
+          </div>
+          <div className="login-input">
+            <div className="login-input-common">
+              <input
+                className="login-input-email"
+                type="text"
+                name="email"
+                placeholder="이메일"
+                autoComplete="off"
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="login-input-common">
+              <input
+                className="login-input-password"
+                type="password"
+                name="password"
+                placeholder="비밀번호"
+                autoComplete="off"
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="login-click-link">
+              <NavLink>이메일찾기</NavLink>
+              <NavLink>비밀번호찾기</NavLink>
+              <NavLink>회원가입</NavLink>
+            </div>
+            <div className="login-button">
+              <button
+                className="login-button-login"
+                onClick={onClickLoginHandler}
+              >
+                로그인
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Login;
