3bdd6ef7f401d2f5f8cc776dfd192d08bd0bba19
spacekingdom <spacekingdom99@gmail.com>
2023-04-13 05:58:12 +0900

    ✨ feat: 미용실 상세정보 조회 구현

--- src/components/beauty/Career.js
+++ src/components/beauty/Career.js
@@ -1,13 +1,34 @@
 import React from "react";
 import "./Career.css";
 
-const Career = () => {
+const Career = (career) => {
+  const handleCertificateNameChange = (event) => {
+    career.setForm({ ...career.form, certificateName: event.target.value });
+  };
+
+  const handleCertificateTextChange = (event) => {
+    career.setForm({ ...career.form, certificateText: event.target.value });
+  };
   return (
     <div className="career">
       <label htmlFor="career">경력/자격</label>
       <div className="career-input">
-        <input type="text" name="career" id="career" placeholder="경력/자격" />
-        <input type="text" name="career" id="career" placeholder="기간/급" />
+        <input
+          type="text"
+          name="career"
+          id="career"
+          placeholder="경력/자격"
+          defaultValue={career.certificateName}
+          onChange={handleCertificateNameChange}
+        />
+        <input
+          type="text"
+          name="career"
+          id="career"
+          placeholder="기간/급"
+          defaultValue={career.certificateText}
+          onChange={handleCertificateTextChange}
+        />
       </div>
     </div>
   );
--- src/components/beauty/HomePage.js
+++ src/components/beauty/HomePage.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./HomePage.css";
 
-const HomePage = () => {
+const HomePage = (web) => {
+  const handleWebChange = (event) => {
+    web.setForm({ ...web.form, web: event.target.value });
+  };
   return (
     <div className="web">
       <label htmlFor="web">홈페이지</label>
       <div className="web-input">
-        <input type="text" name="web" id="web" placeholder="주소" />
+        <input
+          type="text"
+          name="web"
+          id="web"
+          placeholder="주소"
+          defaultValue={web.web}
+          onChange={handleWebChange}
+        />
       </div>
     </div>
   );
